10ea66c0781614f0208fa6349da0560dd2886920
myselfpoulomi <routhpoulomi@gmail.com>
2025-02-24 00:55:01 +0530

    contact me left side

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import email from '../assets/icons/email.svg'
+import location from '../assets/icons/location.svg'
 
 function ContactMe() {
   return (
@@ -9,13 +11,23 @@ function ContactMe() {
           <span className="rounded absolute left-0 bottom-0 w-full h-1 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"></span>
         </h1>
       </div>
-      <div className="h-[880px] border border-white w-[75%] mt-4 flex justify-between">
-            <div className="h-full w-[35%] border border-white">
+      <div className="h-[800px] w-[75%] mt-4 flex justify-between items-center">
+        <div className="h-[400px] w-[35%] flex flex-col gap-7">
+          <span className="text-[50px] font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-fit">
+            Let's Talk
+          </span>
+          <p className="text-zinc-300 text-[17px] font-light">I'm currently avaliable to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
 
-            </div>
-            <div className="h-full w-[60%] border border-white">
-
-            </div>
+          <div className="flex gap-6 items-center">
+            <img src={email} alt="" className="w-[30px]" />
+            <p className="text-zinc-300 text-[17px]">poulomirouth12@gmail.com</p>
+          </div>
+          <div className="flex gap-7 items-center">
+            <img src={location} alt="" className="w-[25px]" />
+            <p className="text-zinc-300 text-[17px]">Kolkata , India</p>
+          </div>
+        </div>
+        <div className="h-full w-[60%] border border-white"></div>
       </div>
     </div>
   );
